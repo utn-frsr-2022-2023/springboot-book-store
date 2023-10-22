c42e05dd2653b1cbf1955dad9dc5e0eec41c4aac
agusb95 <agusbr95@gmail.com>
2023-10-22 19:16:04 -0300

    initial commit

---
+++ src/test/java/utn/book_store/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package utn.book_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
